57ab1a64cf49663d96bec506279b5fa7f7bb478e
김희주 <hejoo345@naver.com>
2021-09-17 15:35:59 +0900

    Add: signUpForm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import LoginForm from '../components/LoginForm'
+import SignUpForm from '../components/SignUpForm'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <LoginForm/>
+      {/* <LoginForm/> */}
+      <SignUpForm/>
     </div>
   )
 }
