cfc6f128aa1fab8ca2bfd119eabe55243de576ca
김희주 <hejoo345@naver.com>
2021-10-04 22:41:32 +0900

    Add: 주석 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from 'next'
+// import CoinInfo from '../components/CoinInfo/CoinInfo'
 import LoginForm from '../components/LoginForm'
 import SignUpForm from '../components/SignUpForm/SignUpForm'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       {/* <LoginForm /> */}
       <SignUpForm />
+      {/* <CoinInfo/> */}
     </div>
   )
 }
