1835fdaffc8fb7acb7ca5121083361931a58ac59
김희주 <hejoo345@naver.com>
2021-10-12 13:24:28 +0900

    Style: News continer max-width 수정

--- src/pages/news/[page].tsx
+++ src/pages/news/[page].tsx
@@ -4,7 +4,7 @@ import { useGetNewsQuery } from '../../features/news/newsSlice';
 import { Grid, Container, Box, CircularProgress} from '@mui/material';
 import NewsItem from '../../components/News/NewsItem';
 import NewsPagination from '../../components/News/NewsPagination';
-
+import { NewsContainer } from '../../styles/pages/newsStyles';
 const Page = () => {
   const router = useRouter();
   const { page } = router.query;
@@ -18,7 +18,7 @@ const Page = () => {
       </Box>
       ) : data ? (
       <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <Container sx={{ px: 0 ,py: 4 }} maxWidth="md">
+        <NewsContainer sx={{ py: 4 }} maxWidth="md">
           <Grid container spacing={4}>
             {data.articles.map((item: any, idx: number) => (
               <NewsItem
@@ -26,7 +26,7 @@ const Page = () => {
               item={item} />
             ))}
           </Grid>
-        </Container>
+        </NewsContainer>
         <NewsPagination pageCounter={Math.ceil(data.totalResults / 15)} page={Number(page)}/>
       </Container>
       ) : null}
---
+++ src/styles/pages/newsStyles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Container } from '@mui/material';
+
+export const NewsContainer = styled(Container)`
+  @media screen and (min-width: 900px) {
+    max-width: 780px;
+  }
+`;
