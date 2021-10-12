68a2a428d7f795ace47472ad8b289bbe133620f6
heejin lee <heejinlee07@gmail.com>
2021-10-12 23:50:25 +0900

    Add: sidesection 간격 조정

--- src/layout/DefaultLayout/DefaultLayoutStyles.ts
+++ src/layout/DefaultLayout/DefaultLayoutStyles.ts
@@ -1,8 +1,13 @@
 import { Button } from '@mui/material';
 import styled from 'styled-components';
 
+import { List, ListItemButton, ListItemText } from '@mui/material';
+import ListSubheader from '@mui/material/ListSubheader';
+import Theme from '../../styles/Theme';
+
 export const SectionWrapper = styled.div`
   min-width: 1440px;
+  max-height: 476px;
   display: flex;
   justify-content: center;
   margin: 10px 0;
@@ -27,4 +32,26 @@ export const SideSection = styled.div`
 
 export const LoginButton = styled(Button)`
   height: 44px;
+`;
+
+export const ListStyle = styled(List)`
+  border: 1px solid ${Theme.palette.secondary.main};
+`;
+
+export const ListSubHeaderStyle = styled(ListSubheader)`
+  border-bottom: 1px solid ${Theme.palette.secondary.light};
+  font-weight: bold;
+  background: #f9f9f9;
+`;
+
+export const ListItemButtonStyle = styled(ListItemButton)`
+  max-width: 198px;
+`;
+
+export const ListItemTextStyle = styled(ListItemText)`
+  & .MuiTypography-root-bWuLxc {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 `;
